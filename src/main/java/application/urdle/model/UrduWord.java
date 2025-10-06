1f8f524d73c21ae7ced0c90e51818e220cfce64e
Taha Murtaza Ali <tahamurtaza21@outlook.com>
2025-10-06 12:16:41 +0100

    Removed Database

--- src/main/java/application/urdle/model/UrduWord.java
+++ src/main/java/application/urdle/model/UrduWord.java
@@ -1,15 +1,15 @@
-package application.urdle.model;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "urdu_words")
-public class UrduWord{
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, length = 5)
-    private String word;
-}
+//package application.urdle.model;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name = "urdu_words")
+//public class UrduWord{
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(nullable = false, length = 5)
+//    private String word;
+//}
--- src/main/java/application/urdle/repository/UrduWordRepository.java
+++ src/main/java/application/urdle/repository/UrduWordRepository.java
@@ -1,10 +1,10 @@
-package application.urdle.repository;
-
-import application.urdle.model.UrduWord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UrduWordRepository extends JpaRepository<UrduWord, Long> {
-    boolean existsByWord(String word);
-}
-
-
+//package application.urdle.repository;
+//
+//import application.urdle.model.UrduWord;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UrduWordRepository extends JpaRepository<UrduWord, Long> {
+//    boolean existsByWord(String word);
+//}
+//
+//
